455fb7b99f4c38ab2f0f49f7c0779d69afb17b74
milan.zivanov <milan.zivanov@gmail.com>
2021-04-26 12:31:16 +0200

    33 Writing more complex JSX code

--- 03-react-basic-working-with-components/src/components/ExspenseItem.js
+++ 03-react-basic-working-with-components/src/components/ExspenseItem.js
@@ -1,5 +1,13 @@
-function ExpenseItem() {
-    return <h2>Expense item!!!</h2>
+const ExpenseItem = () => {
+    return (
+        <div>
+            <div>March 28th 2021</div>
+            <div>
+                <h2>Car Insurence</h2>
+                <div>$290.3</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
