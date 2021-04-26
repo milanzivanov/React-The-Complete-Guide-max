e86dbcde1c1329976b863357b997aa314478cee3
milan.zivanov <milan.zivanov@gmail.com>
2021-04-26 12:21:42 +0200

    32 Building a first custom component

--- 03-react-basic-working-with-components/src/App.js
+++ 03-react-basic-working-with-components/src/App.js
@@ -1,8 +1,12 @@
+import ExpenseItem from "./components/ExspenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
+
   );
 }
 
