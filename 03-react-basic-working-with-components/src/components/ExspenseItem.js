203ef80995e60059c26f65fb6eeae1c6e5d49c5f
milan.zivanov <milan.zivanov@gmail.com>
2021-05-05 10:10:36 +0200

    36 Passing data via "props"

--- 03-react-basic-working-with-components/src/components/ExspenseItem.js
+++ 03-react-basic-working-with-components/src/components/ExspenseItem.js
@@ -1,18 +1,13 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-
-    const expenseDate = new Date(2021, 2, 19);
-    const expenseTitle = "Car insurence";
-    const expenseAmount = 295.58;
-
+const ExpenseItem = (props) => {
 
     return (
         <div className="expense-item">
-            <div>{ expenseDate.toISOString() }</div>
+            <div>{ props.date.toISOString() }</div>
             <div className="expense-item__description">
-                <h2>{ expenseTitle }</h2>
-                <div className="expense-item__price">${ expenseAmount }</div>
+                <h2>{ props.title }</h2>
+                <div className="expense-item__price">${ props.amount }</div>
             </div>
         </div>
     );
