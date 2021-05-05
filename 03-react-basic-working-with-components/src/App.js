a216319668ab4e5eb169b8cf51513efce0598612
milan.zivanov <milan.zivanov@gmail.com>
2021-05-05 11:08:19 +0200

    37 Adding "normal" javascript logic to Components

--- 03-react-basic-working-with-components/src/App.js
+++ 03-react-basic-working-with-components/src/App.js
@@ -3,21 +3,25 @@ import ExpenseItem from "./components/ExspenseItem";
 function App() {
   const expenses = [
     {
+      id: 1,
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
     {
+      id: 2,
       title: "New TV",
       amount: 799.49,
       date: new Date(2021, 2, 12),
     },
     {
+      id: 3,
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
+      id: 4,
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
