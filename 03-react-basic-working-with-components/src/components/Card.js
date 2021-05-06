012474fd987083c6d38a4526c4ddd96b6bc01427
milan.zivanov <milan.zivanov@gmail.com>
2021-05-06 12:13:58 +0200

    39. The Concept of "Composition" ("children props")

---
+++ 03-react-basic-working-with-components/src/components/Card.js
@@ -0,0 +1,14 @@
+import './Card.css';
+
+function Card(props) {
+    const classess = " card " + props.className;
+    
+    return (
+        <div className={classess}>
+            {props.children}
+        </div>
+    )
+
+}
+
+export default Card;
